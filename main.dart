b9190ddd8baa995c8c4c680f96dbd071118a417e
arafay493 <arafay493gmail.com>
2022-03-12 23:35:51 +0500

    Done

---
+++ main.dart
@@ -0,0 +1,65 @@
+// void main() {
+//   print("Hello world");
+// }
+
+// import 'dart:io';
+
+// void main() {
+//   print("Please Enter Email");
+//   var email = stdin.readLineSync();
+//   print("Enter Password");
+//   var password = stdin.readLineSync();
+
+//   if (email == "abc@gmail.com" && password == '1234567') {
+//     print("Welcome \n You are logged in");
+//   } else if (email != "abc@gmail.com" && password == '1234567') {
+//     print("Enter the correct email");
+//   } else if (password != '1234567' && email == "abc@gmail.com") {
+//     print("Enter the correct password");
+//   } else {
+//     print("Please enter the correct email and password");
+//   }
+// }
+
+// void main() {
+//   // List
+
+//   // var studentNames = ["cricket", 123, true];
+//   // List<int> studentRoll = [1, 2, 5, 6];
+//   // List<String> studentNames1 = ["Basit" , "Bilal", "Nazir"];
+//   // List<dynamic> studentNames2 = ["Basit" , 123, "Nazir"];
+
+//   List<int> marks = [60, 70, 35, 40, 85, 90];
+//   // print(marks);
+//   // print(marks.length);
+//   // marks.replaceRange(2, 5, [65, 85]);
+//   // print(marks);
+//   // marks.insert(3, 50);
+//   // print(marks);
+//   // marks.insertAll(3, [50 , 65  , 85]);
+//   // print(marks);
+//   // print(marks.first);
+//   // print(marks.last);
+//   // marks.add(25);
+//   // print(marks);
+//   // marks.addAll([25 ,45 , 55]);
+//   // print(marks);
+//   // marks.remove(70);
+//   // print(marks);
+//   // marks.removeAt(4);
+//   // print(marks);
+//   // print(marks);
+//   // print(marks.reversed);
+//   // marks.sort();
+//   // print(marks);
+
+//   // var rev = marks.reversed;
+//   // print(rev);
+//   // var revList = List.of(rev);
+//   // print(revList);
+//   // revList.sort();
+//   // print(revList);
+// }
+
+// import 'dart:io';
+
